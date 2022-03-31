7f4ebac4b6754e5363adcd3824a47846540a18cc
ace777j <axe_tazlok126@hotmail.com>
2022-03-31 11:49:15 -0500

    Comienzo

---
+++ pid_Grupo01/src/main/java/com/empresa/entity/Departamento.java
@@ -0,0 +1,32 @@
+package com.empresa.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "departamento")
+@Getter
+@Setter
+public class Departamento {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idDepartamento;
+	private String piso;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Temporal(TemporalType.DATE)
+	private Date fechaRenta;
+
+}
